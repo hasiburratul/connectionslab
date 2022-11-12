88a9d7c8d7ca9f9989f5e7a4bb2018f078e7ec3b
hasiburratul <hasibur.ratul@gmail.com>
2022-11-12 22:54:49 +0400

    commit

---
+++ Week_10/Project_2/public/js/bullet.js
@@ -0,0 +1,11 @@
+let Bullet = function(angle,xCoord,yCoord) {
+    this.angle = angle + Math.PI/2;
+    this.coordinates = {x: xCoord, y: yCoord};
+    this.moveX = Math.cos(angle - Math.PI/2);
+    this.moveY = Math.sin(angle - Math.PI/2);
+  };
+  
+  Bullet.prototype.move = function() {
+    this.coordinates.x += 5*this.moveX;
+    this.coordinates.y += 5*this.moveY;
+  };
